2967d125aa1b81cbfe7b199d0ab3cbb7965e4ffb
birdheee <shrt1138@gmail.com>
2023-06-28 09:36:31 +0900

    Lotto girl~~

--- src/p02/Lotto.java
+++ src/p02/Lotto.java
@@ -1,21 +1,13 @@
 package p02;
 
-import java.util.Random;
-
 public class Lotto {
 
 	public static void main(String[] args) {
 		int[] lotto = new int[6];
 		
 		// Math.random으로
-//		for(int i=0; i<lotto.length; i++) { // 1부터 45까지 랜덤으로 6번
-//			lotto[i] = (int)(45 * Math.random() + 1); //Math.random은 double 형태이고 범위가 0부터 0.9999... 까지임
-//		}
-		
-		// util.Random으로
-		Random ra = new Random();
-		for(int i=0; i<lotto.length; i++) {
-			lotto[i] = ra.nextInt(45) + 1; // 범위가 0부터 44까지이기 때문에 +1을 함 
+		for(int i=0; i<lotto.length; i++) { // 1부터 45까지 랜덤으로 6번
+			lotto[i] = (int)(45 * Math.random() + 1); //Math.random은 double 형태이고 범위가 0부터 0.9999... 까지임
 		}
 		
 		// 출력
